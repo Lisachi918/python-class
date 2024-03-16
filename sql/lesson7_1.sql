5ba5cdeeca8267e204686f98e9b5aa0542402bb6
曾子綺 <161096286+Lisachi918@users.noreply.github.com>
2024-03-16 10:40:30 +0800

    Create lesson7_1.sql

---
+++ sql/lesson7_1.sql
@@ -0,0 +1,45 @@
+/*進站人數最多的一筆*/
+
+SELECT *
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
+WHERE 進站人數 = 82586
+
+SELECT MAX(進站人數)
+FROM gate_count;
+
+/*用subquery把上面的兩行敘述變成一行表示*/
+SELECT *
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
+WHERE 進站人數 = (
+	SELECT MAX(進站人數)
+	FROM gate_count
+);
+
+/*各站點人數最多的一筆*/
+
+SELECT 站點編號
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號;
+
+SELECT 站點編號,MAX(進站人數)
+FROM gate_count
+GROUP BY 站點編號;
+
+SELECT 站點編號
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
+WHERE (站點編號,進站人數) IN(
+	SELECT 站點編號,MAX(進站人數)
+	FROM gate_count
+	GROUP BY 站點編號
+);
+/*GROUP BY 站點編號,進站人數*/
+
+SELECT 站點編號,MAX(進站人數)
+FROM gate_count
+GROUP BY 站點編號;
+     
+
+
+
+
+
+
